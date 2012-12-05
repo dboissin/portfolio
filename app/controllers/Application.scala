cbc8663dd0e614f6f74317ffcb8c2dfa48f73f19
dboissin <damien.boissin@gmail.com>
2012-12-05 01:05:54 +0100

    [Evo] bootstrap wedding site

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,23 +3,31 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-import utils._
-
-import java.io.File
-
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index())
   }
 
-  def test = Action {
-    val photo = new File(Photos.rootImages + Fake.findPhoto(319).get.path)
-    val smallPhoto = File.createTempFile("blip", ".jpg")
-    ImageUtils.resize(photo, smallPhoto, 930)
-    Logger.debug(ImageUtils.extractMetadata(photo))
-    Logger.debug(ImageUtils.jsonFaceDetection(smallPhoto).toString)
-    Ok.sendFile(smallPhoto, true)
+  def story = Action {
+    Ok(views.html.story())
+  }
+
+  def wedding = Action {
+    Ok(views.html.wedding())
+  }
+
+  def location = Action {
+    Ok(views.html.location())
+  }
+
+  def guestbook = Action {
+    Ok(views.html.guestbook())
+  }
+
+  def contact = Action {
+    Ok(views.html.contact())
   }
 
 }
+
