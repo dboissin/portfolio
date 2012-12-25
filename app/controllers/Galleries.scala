37018d6fd2f20e4adab331cb3cfa8ea14cea4f16
Damien Boissin <dbo@zenexity.com>
2012-12-25 21:13:41 +0100

    [Evo] create gallery

--- app/controllers/Galleries.scala
+++ app/controllers/Galleries.scala
@@ -2,9 +2,27 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import scala.slick.driver.PostgresDriver.simple._
+import Database.threadLocalSession
+import play.api.db.DB
+import play.api.Play.current
+
 import models.{ Gallery, Galleries => Gals }
 
 object Galleries extends Controller {
+
+  lazy val database = Database.forDataSource(DB.getDataSource())
+
+  val galleryForm = Form(
+    mapping(
+      "gallery.id" -> optional(longNumber),
+      "gallery.name" -> nonEmptyText,
+      "gallery.description" -> optional(text)
+    )(Gallery)(Gallery.unapply _)
+  )
 
   def index = Action {
     val galleries = Fake.galleries
@@ -17,5 +35,21 @@ object Galleries extends Controller {
       Ok(views.html.galleries.gallery(gallery, photos))
     }.getOrElse(NotFound)
   }
+
+  def addGallery = Action { implicit request =>
+    galleryForm.bindFromRequest.fold(
+      errors => {
+        Logger.debug(errors.toString)
+        BadRequest
+      },
+      gallery => {
+        database.withSession {
+          Gals.insert(gallery)
+          Ok
+        }
+      }
+    )
+  }
+
 }
 
